2737d755db06615d86c2533d95b90d9668bc411f
shaheerbits <shaheercodes.allday@gmail.com>
2026-05-14 16:45:42 +0530

    Minor Fix

--- union.zig
+++ union.zig
@@ -4,30 +4,34 @@ const Circle = struct {
     radius: f32,
 
     pub fn area(self: *const Circle) f32 {
-        return 2 * self.radius * self.radius;
+        return std.math.pi * self.radius * self.radius;
     }
 };
 
 const Rectangle = struct {
-    width: f32,
-    height: f32,
+    length: f32,
+    breadth: f32,
 
     pub fn area(self: *const Rectangle) f32 {
-        return self.width * self.height;
+        return self.length * self.breadth;
     }
 };
 
-fn areaOf(shape: anytype) !f32 {
-    if (@hasDecl(shape, "area")) return shape.area();
-    @compileError("Invalid Shape!");
-}
+const Shape = union(enum) {
+    circle: Circle,
+    rectangle: Rectangle,
+
+    pub fn area(self: Shape) f32 {
+        return switch (self) {
+            .circle => |c| c.area(),
+            .rectangle => |r| r.area(),
+        };
+    }
+};
 
 pub fn main() !void {
-    const c1 = Circle{ .radius = 3.2 };
-    const r1 = Rectangle{ .width = 4, .height = 5.6 };
+    const c1 = Shape{ .circle = .{ .radius = 3.2 } };
+    const r1 = Shape{ .rectangle = .{ .length = 4, .breadth = 5.6 } };
 
-    const areaOfC1 = try areaOf(c1);
-    const areaOfR1 = try areaOf(r1);
-
-    std.debug.print("Circle Area: {d}\nRectangle Area: {d}\n", .{ areaOfC1, areaOfR1 });
+    std.debug.print("Circle Area: {d}\nRectangle Area: {d}\n", .{ c1.area(), r1.area() });
 }
